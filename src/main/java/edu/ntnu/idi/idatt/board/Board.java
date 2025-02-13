c58139d72ac7f3cf67d8ec4e2db3df44a8d424f4
aMoltu <aleksandermoltu@gmail.com>
2025-02-13 12:57:37 +0100

    feat: implement Board and Tile

--- src/main/java/edu/ntnu/idi/idatt/board/Board.java
+++ src/main/java/edu/ntnu/idi/idatt/board/Board.java
@@ -1,5 +1,21 @@
 package edu.ntnu.idi.idatt.board;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Board {
 
+  private final Map<Integer, Tile> tiles;
+
+  Board() {
+    tiles = new HashMap<>();
+  }
+
+  public void addTile(Tile tile) {
+    tiles.put(tile.getTileId(), tile);
+  }
+
+  public Tile getTile(int tileId) {
+    return tiles.get(tileId);
+  }
 }
--- src/main/java/edu/ntnu/idi/idatt/board/Tile.java
+++ src/main/java/edu/ntnu/idi/idatt/board/Tile.java
@@ -1,5 +1,33 @@
 package edu.ntnu.idi.idatt.board;
 
+import edu.ntnu.idi.idatt.actions.LadderAction;
+import edu.ntnu.idi.idatt.actions.TileAction;
+import edu.ntnu.idi.idatt.game.Player;
+
 public class Tile {
 
+  private Tile nextTile;
+  private int tileId;
+  private TileAction landAction;
+
+  public Tile(int tileId) {
+    this.tileId = tileId;
+  }
+
+  public void landPlayer(Player player) {
+    landAction = new LadderAction();
+    landAction.perform(player);
+  }
+
+  public void leavePlayer(Player player) {
+    player.placeOnTile(nextTile);
+  }
+
+  public void setNextTile(Tile nextTile) {
+    this.nextTile = nextTile;
+  }
+
+  public int getTileId() {
+    return tileId;
+  }
 }
