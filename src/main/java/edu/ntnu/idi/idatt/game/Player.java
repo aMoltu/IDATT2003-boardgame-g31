8c554cab82281d9c8fa5468eaf14213879743dab
AlexanderArne <145281135+AlexanderArne@users.noreply.github.com>
2025-02-13 12:57:03 +0100

    feat(Dice): continued implementation of Dice docs: added more javadoc to Player and BoardGame

--- src/main/java/edu/ntnu/idi/idatt/game/Player.java
+++ src/main/java/edu/ntnu/idi/idatt/game/Player.java
@@ -2,21 +2,43 @@ package edu.ntnu.idi.idatt.game;
 
 import edu.ntnu.idi.idatt.board.Tile;
 
+/** Class representing a player of the game.
+ *
+ */
 public class Player {
-  private String name;
+  private final String name;
   private Tile currentTile;
 
+  /**
+   * Constructor for the Player class.
+   * Assigns name and game.
+   *
+   * @param name String representing the player's name.
+   * @param game BoardGame
+   */
   public Player(String name, BoardGame game) {
     this.name = name;
   }
 
+  /** Places player on a specific tile.
+   *
+   * @param tile that player gets placed on.
+   */
   public void placeOnTile(Tile tile) {
-    this.currentTile = tile;
+    currentTile = tile;
   }
 
+  /** moves the player a given amount of tiles.
+   *
+   * @param steps int number of tiles the player moves.
+   */
   public void move(int steps) {
     for (int i = 0; i < steps; i++) {
       currentTile = currentTile.getNextTile();
     }
+  }
+
+  public String getName() {
+    return name;
   }
 }
