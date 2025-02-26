63b29696d9d610f188931c6de59e53496c4cba69
aMoltu <aleksandermoltu@gmail.com>
2025-02-26 20:40:27 +0100

    feat: add text based user interface and gameplay

--- src/main/java/edu/ntnu/idi/idatt/Main.java
+++ src/main/java/edu/ntnu/idi/idatt/Main.java
@@ -1,18 +1,43 @@
 package edu.ntnu.idi.idatt;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import edu.ntnu.idi.idatt.game.BoardGame;
+import edu.ntnu.idi.idatt.game.Player;
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    System.out.printf("Hello and welcome!");
+    BoardGame game = new BoardGame();
+    Scanner scanner = new Scanner(System.in);
 
-    for (int i = 1; i <= 5; i++) {
-      //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-      // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-      System.out.println("i = " + i);
+    //add between 1 and 4 players
+    int playerCount = 1;
+    boolean addMorePlayers = true;
+    while (playerCount < 5 && addMorePlayers) {
+      System.out.print("Type the name of player " + playerCount + ": ");
+      String name = scanner.nextLine();
+      game.addPlayer(new Player(name, game));
+      playerCount++;
+
+      if (playerCount < 5) {
+        String command = "";
+        while (!command.equals("y") && !command.equals("n")) {
+          System.out.print("Do you want to add more players? y/n: ");
+          command = scanner.nextLine();
+        }
+        if (command.equals("n")) {
+          addMorePlayers = false;
+        }
+      }
     }
+
+    System.out.println("\nAnd the game begins!\n");
+
+    while (game.getWinner() == null) {
+      game.play();
+      scanner.nextLine();
+    }
+
+    System.out.println("And the winner is " + game.getWinner().getName());
   }
 }
