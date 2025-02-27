5734eb5b6dc19e1c144b4bf67a0443bf0e4a01a3
AlexanderArne <145281135+AlexanderArne@users.noreply.github.com>
2025-02-27 10:23:46 +0100

    doc: Added some Javadoc to Die

--- src/main/java/edu/ntnu/idi/idatt/dice/Die.java
+++ src/main/java/edu/ntnu/idi/idatt/dice/Die.java
@@ -2,11 +2,18 @@ package edu.ntnu.idi.idatt.dice;
 
 import java.util.Random;
 
+/**
+ * This class represents a single six sided die, and contains method to roll and read the value.
+ */
 public class Die {
   private int lastRolledValue;
   private final Random rand = new Random();
 
-
+  /**
+   * Rolling the die will change the shown value to a new random one between 1 and 6.
+   *
+   * @return the new value of the die.
+   */
   public int roll() {
     lastRolledValue = rand.nextInt(1, 7);
     return lastRolledValue;
