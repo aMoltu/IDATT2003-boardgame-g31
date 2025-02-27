2a7d7a33f8337c1e25dae35ecc5e7b9d2e05a7f7
aMoltu <aleksandermoltu@gmail.com>
2025-02-27 13:03:27 +0100

    test: add tests

---
+++ src/test/java/edu/ntnu/idi/idatt/game/BoardGameTest.java
@@ -0,0 +1,42 @@
+package edu.ntnu.idi.idatt.game;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import edu.ntnu.idi.idatt.board.Tile;
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for {@link Player}.
+ */
+public class BoardGameTest {
+
+  private BoardGame game;
+
+  @BeforeEach
+  void init() {
+    game = new BoardGame();
+  }
+
+  @Test
+  void testConstructor() {
+    assertNotNull(game.getBoard().getTile(5));
+  }
+
+  @Test
+  void testAddingPlayers() {
+    Player p1 = new Player("P1");
+    Player p2 = new Player("P2");
+    game.addPlayer(p1);
+    game.addPlayer(p2);
+
+    ArrayList<Player> players = game.getPlayers();
+
+    assertNotNull(players);
+    assertEquals(2, players.size());
+    assertEquals(p1, players.get(0));
+    assertEquals(p2, players.get(1));
+  }
+}
---
+++ src/test/java/edu/ntnu/idi/idatt/game/PlayerTest.java
@@ -0,0 +1,39 @@
+package edu.ntnu.idi.idatt.game;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import edu.ntnu.idi.idatt.board.Tile;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for {@link Player}.
+ */
+public class PlayerTest {
+
+  @Test
+  void testPlaceOnTile() {
+    Tile tile = new Tile(0);
+    Player player = new Player("name");
+
+    player.placeOnTile(tile);
+
+    assertEquals(tile, player.getCurrentTile());
+  }
+
+  @Test
+  void testMove() {
+    Tile tile1 = new Tile(0);
+    Tile tile2 = new Tile(1);
+    Tile tile3 = new Tile(2);
+    tile1.setNextTile(tile2);
+    tile2.setNextTile(tile3);
+
+    Player player = new Player("name");
+    player.placeOnTile(tile1);
+
+    player.move(2);
+
+    assertEquals(tile3, player.getCurrentTile());
+  }
+}
+
