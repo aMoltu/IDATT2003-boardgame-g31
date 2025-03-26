9e28b84664ea25f4cd400235ccd4cf1d52b50b9c
aMoltu <aleksandermoltu@gmail.com>
2025-03-26 12:57:40 +0100

    feat: add player csv writer

---
+++ src/main/java/edu/ntnu/idi/idatt/fileio/PlayerCsvWriter.java
@@ -0,0 +1,31 @@
+package edu.ntnu.idi.idatt.fileio;
+
+import edu.ntnu.idi.idatt.game.Player;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+/**
+ * Class for writing player information to CSV files.
+ */
+public class PlayerCsvWriter {
+
+  /**
+   * Write player names on separate lines.
+   *
+   * @param path    path to the output file
+   * @param players list of players with a name
+   */
+  public static void write(Path path, List<Player> players) {
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(path.toFile()))) {
+      for (Player player : players) {
+        writer.write(player.getName());
+        writer.newLine();
+      }
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    }
+  }
+}
