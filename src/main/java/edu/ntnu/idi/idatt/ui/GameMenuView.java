95010ba9b688f59c4c53ad6c20b1f10a2b1b65cb
aMoltu <aleksandermoltu@gmail.com>
2025-05-05 13:37:03 +0200

    feat: separate player selection from game selection

--- src/main/java/edu/ntnu/idi/idatt/ui/GameMenuView.java
+++ src/main/java/edu/ntnu/idi/idatt/ui/GameMenuView.java
@@ -7,6 +7,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
@@ -15,26 +16,35 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
 public class GameMenuView {
-  private final BorderPane root;
+
+  private final StackPane root;
   private final GameMenuController controller;
   private final ListView<String> playerListView = new ListView<>();
+  private final BorderPane gameSelectScreen;
+  private final BorderPane playerSelectScreen;
   private final ObservableList<String> observablePlayerList = FXCollections.observableArrayList();
   private ComboBox<String> boardSelector;
 
   public GameMenuView(GameMenuController controller) {
     this.controller = controller;
-    root = new BorderPane();
-    setUpMenuScreen();
+    gameSelectScreen = generateGameSelectScreen();
+    playerSelectScreen = generatePlayerSelectScreen();
+
+    root = new StackPane();
+    root.getChildren().setAll(gameSelectScreen);
   }
 
-  private void setUpMenuScreen() {
-    root.setStyle("-fx-background-color: #eee;");
+  private BorderPane generateGameSelectScreen() {
+    BorderPane ret = new BorderPane();
+    ret.setStyle("-fx-background-color: #eee;");
 
     //Title section
     Text title = new Text("Game Menu");
@@ -77,6 +87,48 @@ public class GameMenuView {
     boardSelector.setMinWidth(200);
     boardSelector.setOnAction(event -> controller.setSelectedBoard(boardSelector.getValue()));
 
+    // Button that goes to player select screen
+    Button startGameButton = new Button("Continue");
+    startGameButton.setStyle("-fx-background-color: #4caf50;");
+    startGameButton.setPrefWidth(200);
+    startGameButton.setPrefHeight(40);
+    startGameButton.setFont(Font.font("System", FontWeight.BOLD, 14));
+
+    startGameButton.setOnAction(event -> root.getChildren().setAll(playerSelectScreen));
+
+    //Add all components to the content box
+    contentBox.getChildren().addAll(gameSelectionTitle, gameSelector,
+        boardSelectionTitle, boardSelector, new Separator(), startGameButton);
+
+    ret.setTop(titleBox);
+    ret.setCenter(contentBox);
+    return ret;
+  }
+
+  private BorderPane generatePlayerSelectScreen() {
+    BorderPane ret = new BorderPane();
+    ret.setStyle("-fx-background-color: #eee;");
+
+    //Go back button
+    Button goBackBtn = new Button("go back");
+    goBackBtn.setOnAction(event -> root.getChildren().setAll(gameSelectScreen));
+    goBackBtn.setAlignment(Pos.TOP_LEFT);
+
+    //Title section
+    Text title = new Text("Game Menu");
+    title.setFont(Font.font("System", FontWeight.BOLD, 24));
+    HBox titleBox = new HBox(title);
+    titleBox.setAlignment(Pos.CENTER);
+    titleBox.setPadding(new Insets(20, 0, 30, 0));
+
+    VBox topBox = new VBox();
+    topBox.getChildren().addAll(goBackBtn, titleBox);
+
+    //Main content section
+    VBox contentBox = new VBox(15);
+    contentBox.setAlignment(Pos.TOP_CENTER);
+    contentBox.setPadding(new Insets(10, 50, 10, 50));
+
     //Player section
     Text playerSectionTitle = new Text("Add players");
     playerSectionTitle.setFont(Font.font("System", FontWeight.BOLD, 16));
@@ -104,9 +156,11 @@ public class GameMenuView {
     playerListView.getSelectionModel().selectedItemProperty().addListener(
         (observable, oldValue, newValue) -> removePlayerButton.setDisable(newValue == null));
 
-    addPlayerButton.setOnAction(event -> controller.addPlayer(playerNameField, observablePlayerList));
+    addPlayerButton.setOnAction(
+        event -> controller.addPlayer(playerNameField, observablePlayerList));
 
-    removePlayerButton.setOnAction(event -> controller.removePlayer(playerListView, observablePlayerList));
+    removePlayerButton.setOnAction(
+        event -> controller.removePlayer(playerListView, observablePlayerList));
 
     //Start game Button
     Button startGameButton = new Button("Start Game");
@@ -118,16 +172,16 @@ public class GameMenuView {
     startGameButton.setOnAction(event -> controller.startGame());
 
     //Add all components to the content box
-    contentBox.getChildren().addAll(gameSelectionTitle, gameSelector,
-        boardSelectionTitle, boardSelector,
-        playerSectionTitle, playerInputBox, playerListView,
+    contentBox.getChildren().addAll(playerSectionTitle, playerInputBox, playerListView,
         removePlayerButton, new Separator(), startGameButton);
 
-    root.setTop(titleBox);
-    root.setCenter(contentBox);
+    ret.setTop(topBox);
+    ret.setCenter(contentBox);
+
+    return ret;
   }
 
-  public BorderPane getRoot() {
+  public StackPane getRoot() {
     return root;
   }
 }
