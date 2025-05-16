961d2579c07ae8e223e97a6041d96f15e3e629a4
aMoltu <aleksandermoltu@gmail.com>
2025-05-16 17:26:56 +0200

    style: make window bigger and prevent auto resizing

--- src/main/java/edu/ntnu/idi/idatt/ui/SceneController.java
+++ src/main/java/edu/ntnu/idi/idatt/ui/SceneController.java
@@ -9,11 +9,15 @@ public class SceneController {
 
   Stage stage;
   HashMap<String, Scene> scenes;
+  int defaultWidth = 1000;
+  int defaultHeight = 600;
 
   public SceneController(Stage stage, Scene initialScene) {
     scenes = new HashMap<>();
     this.stage = stage;
     this.stage.setScene(initialScene);
+    stage.setWidth(defaultWidth);
+    stage.setHeight(defaultHeight);
     stage.show();
   }
 
@@ -24,7 +28,12 @@ public class SceneController {
   public void setScene(String name) {
     Scene scene = scenes.get(name);
     if (scene != null) {
+      double prevWidth = stage.getWidth();
+      double prevHeight = stage.getHeight();
+
       stage.setScene(scene);
+      stage.setWidth(prevWidth);
+      stage.setHeight(prevHeight);
     } else {
       System.err.println("No scene with name " + name + " found");
     }
