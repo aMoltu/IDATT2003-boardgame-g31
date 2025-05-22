3103a34a37c1c562319818c9a42dda27b8029f76
aMoltu <aleksandermoltu@gmail.com>
2025-05-22 21:55:06 +0200

    docs: add javadoc comment

--- src/test/java/edu/ntnu/idi/idatt/model/RollAgainTest.java
+++ src/test/java/edu/ntnu/idi/idatt/model/RollAgainTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Unit tests for {@link Tile}.
+ */
 public class RollAgainTest {
 
   @Test
